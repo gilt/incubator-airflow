57093142673a9faa52fb55601c09bd032bccc890
Daniel Pires <dmateusp@gmail.com>
2017-11-22 15:21:38 +0000

    fixing ecr log in issue

--- airflow/hbc_deployment/scripts/deployment/ecs/build_push_image.sh
+++ airflow/hbc_deployment/scripts/deployment/ecs/build_push_image.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
+eval $(aws --region us-east-1 ecr get-login)
 docker build -t 326027360148.dkr.ecr.us-east-1.amazonaws.com/airflow:latest .  > /dev/null
 docker push 326027360148.dkr.ecr.us-east-1.amazonaws.com/airflow:latest > /dev/null

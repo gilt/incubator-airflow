2b9ea90df7f9b17475d225954904e181d45759df
Daniel Pires <dmateusp@gmail.com>
2017-11-22 11:51:46 +0000

    Getting AWS credentials in automated build

--- airflow/hbc_deployment/scripts/deployment/ecs/build_push_image.sh
+++ airflow/hbc_deployment/scripts/deployment/ecs/build_push_image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build --build-arg AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID} --build-arg AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY} -t 326027360148.dkr.ecr.us-east-1.amazonaws.com/airflow:latest .  > /dev/null
+docker build -t 326027360148.dkr.ecr.us-east-1.amazonaws.com/airflow:latest .  > /dev/null
 docker push 326027360148.dkr.ecr.us-east-1.amazonaws.com/airflow:latest

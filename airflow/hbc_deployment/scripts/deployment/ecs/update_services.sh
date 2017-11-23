5eb4579ca68a6000e3212e3cc82b7dcd2e417f21
Daniel Pires <dmateusp@gmail.com>
2017-11-23 11:41:05 +0000

    correcting task definition update

--- airflow/hbc_deployment/scripts/deployment/ecs/update_services.sh
+++ airflow/hbc_deployment/scripts/deployment/ecs/update_services.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-aws ecs update-service --cluster airflow --service airflow-worker-prod > /dev/null
-aws ecs update-service --cluster airflow --service airflow-master-prod > /dev/null
+aws ecs update-service --cluster airflow --service airflow-worker-prod --task-definition airflow-worker > /dev/null
+aws ecs update-service --cluster airflow --service airflow-master-prod --task-definition airflow-master > /dev/null

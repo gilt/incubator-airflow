da034567c5761be8003967157aaaedc9e341ceba
Daniel Pires <dmateusp@gmail.com>
2017-11-23 13:18:08 +0000

    adding --no-cache flag otherwise new changes can be missed on pip install

--- airflow/hbc_deployment/scripts/deployment/ecs/build_push_image.sh
+++ airflow/hbc_deployment/scripts/deployment/ecs/build_push_image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 eval $(aws --region us-east-1 ecr get-login --no-include-email)
-docker build -t 326027360148.dkr.ecr.us-east-1.amazonaws.com/airflow:latest .
+docker build --no-cache -t 326027360148.dkr.ecr.us-east-1.amazonaws.com/airflow:latest .
 docker push 326027360148.dkr.ecr.us-east-1.amazonaws.com/airflow:latest

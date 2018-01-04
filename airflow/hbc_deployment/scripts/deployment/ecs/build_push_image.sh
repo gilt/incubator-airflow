7917da09b94c7faf0875bead7cd787efe18aee0b
Daniel Pires <dmateusp@gmail.com>
2018-01-04 16:25:04 +0000

    [HBC_CUSTOM] using CACHEBUST to cache all but the PIP command

--- airflow/hbc_deployment/scripts/deployment/ecs/build_push_image.sh
+++ airflow/hbc_deployment/scripts/deployment/ecs/build_push_image.sh
@@ -4,6 +4,6 @@ TAG=$1
 
 {
   eval $(aws --region us-east-1 ecr get-login --no-include-email)
-  docker build --no-cache -t 326027360148.dkr.ecr.us-east-1.amazonaws.com/airflow:${TAG} .
+  docker build --build-arg CACHEBUST=$(date +%s) -t 326027360148.dkr.ecr.us-east-1.amazonaws.com/airflow:${TAG} .
   docker push 326027360148.dkr.ecr.us-east-1.amazonaws.com/airflow:${TAG}
 } || exit 1
